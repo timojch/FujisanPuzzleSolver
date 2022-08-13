b39d745585a6073e2e84e13265b99a6c13b26677
timojch <timo.ch@outlook.com>
2022-08-13 01:26:02 -0700

    Updated for Wednesday's problem.

--- FujisanPuzzleSolver/Move.cs
+++ FujisanPuzzleSolver/Move.cs
@@ -9,12 +9,32 @@ internal class Move
 {
     public int PriestNumber { get; init; }
 
+    public char PriestLetter { get => (char)(this.PriestNumber + 'A'); }
+
     public Tuple<int, int> StartingLocation { get; init; }
 
     public Tuple<int, int> EndingLocation { get; init; }
 
     public override string ToString()
     {
-        return $">! Move Priest {(char)(this.PriestNumber + 'A')} to the {(this.EndingLocation.Item2 == 0 ? "top" : "bottom")} of domino {this.EndingLocation.Item1}  ";
+        return $"Move Priest {this.PriestLetter} to the {(this.EndingLocation.Item2 == 0 ? "top" : "bottom")} of domino {this.EndingLocation.Item1}  ";
+    }
+
+    public string ToShortString(int[,] board)
+    {
+        int dx = this.EndingLocation.Item1 - this.StartingLocation.Item1;
+        int dy = this.EndingLocation.Item2 - this.StartingLocation.Item2;
+        if (dy != 0)
+        {
+            return $"{this.PriestLetter}";
+        }
+        else if (dx > 0)
+        {
+            return $"{this.PriestLetter}>{board[this.EndingLocation.Item1, this.EndingLocation.Item2]}";
+        }
+        else
+        {
+            return $"{this.PriestLetter}<{board[this.EndingLocation.Item1, this.EndingLocation.Item2]}";
+        }
     }
 }
